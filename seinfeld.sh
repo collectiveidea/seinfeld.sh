137164e1ff53e2bcf914eab3aef30f540ee68ab7
Brian Ryckbost <bryckbost@gmail.com>
2011-08-20 22:54:01 -0400

    better date regex

--- seinfeld.sh
+++ seinfeld.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s 'http://calendaraboutnothing.com/~bryckbost.json' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed '/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]"]/!d' | sed -e 's/["]]*/''/g'
+curl -s 'http://calendaraboutnothing.com/~bryckbost.json' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed '/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}"]/!d' | sed -e 's/["]]*/''/g'
